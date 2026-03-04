1ff4b7779a8cdfa7a14211294846eb6e53db488d
Engineer <engineer@kuberna.labs>
2026-03-04 09:04:08 +0200

    chore(backend): add jest config

---
+++ backend/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.ts'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js'],
+    clearMocks: true,
+};
+
